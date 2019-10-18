f7cbae42e9a9c7851352a464ab77b0fe1e5434a4
Nick Engelhardt <engelhardtnick@gmail.com>
2019-10-18 21:16:07 +0200

    "Add sudo" function from https://github.com/sorin-ionescu/prezto

--- zsh/widgets.zsh
+++ zsh/widgets.zsh
@@ -6,5 +6,15 @@ function _print-pwd {
 zle -N _print-pwd
 bindkey "^W" _print-pwd
 
+# add 'sudo ' at the beginning of the line
+function _prepend-sudo() {
+  if [[ "$BUFFER" != su(do|)\ * ]]; then
+    BUFFER="sudo $BUFFER"
+    (( CURSOR += 5 ))
+  fi
+}
+zle -N _prepend-sudo
+bindkey "^F" _prepend-sudo
+
 # kill last word on ctrl+backspace
 bindkey "^H" backward-kill-word
