e898cee3996896041f236acbd00dbc0f64e5b9c1
Nick Engelhardt <engelhardtnick@gmail.com>
2019-10-20 14:46:42 +0200

    Move sourcing to .zshrc in home and zsh settings to its own file

---
+++ zsh/zsh_settings.zsh
@@ -0,0 +1,12 @@
+ZSH_THEME="robbyrussell"
+
+# use hyphen-insensitive completion.
+HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to enable command auto-correction.
+ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
+
+plugins=(git)
