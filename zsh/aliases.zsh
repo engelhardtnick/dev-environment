62109f4b9ff9142d88d1fa9a31c780c5baf97432
Nick Engelhardt <nick.engelhardt@bmw.de>
2019-12-10 10:00:40 +0100

    Add alias to copy to clipboard

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # software aliases
 alias -g GREP="| grep -i"
 alias -g GREP5="| grep -ic 5"
+alias -g CLIP='| xclip -selection c'
 
 alias pycharm="pycharm.sh 2>/dev/null &"
 alias idea="idea.sh 2>/dev/null &"
