195580f8ed83ea3275f55a110e73c1d5904d626d
Nick Engelhardt <engelhardtnick@gmail.com>
2020-06-27 18:18:11 +0200

    Add dell-xps specific env_vars

---
+++ dell-xps-specific/env_vars.zsh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/home/nick/anaconda3/bin
+export PATH="/opt/idea-IC-192.6817.14/bin:$PATH"
+export PATH="/opt/FreeFileSync/Bin:$PATH"
+export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="/opt/pycharm-community-2020.1/bin:$PATH"
