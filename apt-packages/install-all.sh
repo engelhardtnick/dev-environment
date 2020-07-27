99b14d0d5a8dad4d1074143080cb957bb7eb2b96
Nick Engelhardt <nick.engelhardt@bmw.de>
2020-07-27 18:03:43 +0200

    Add file to install apt packages

---
+++ apt-packages/install-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+sudo apt install -y vim git zsh python3.7* python3.6* zim gnome-shell gnome-shell-common gnome-shell-extensions \
+gnome-tweaks
+
+#TODO install and configure ohmyzsh
+#TODO install atom
+#TODO install and configure arc application launcher
+#TODO configure gnome everything
