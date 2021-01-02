2ac2add2b5eeeb37e22f880a6770d2beb8865466
Nick Engelhardt <engelhardtnick@gmail.com>
2021-01-02 12:05:23 +0100

    Update pycharm and idea vars

--- dell-xps-specific/env_vars.zsh
+++ dell-xps-specific/env_vars.zsh
@@ -1,8 +1,6 @@
-export PATH=$PATH:/home/nick/anaconda3/bin
-export PATH="/opt/idea-IC-192.6817.14/bin:$PATH"
+export PATH="/opt/idea/bin:$PATH"
 export PATH="/opt/FreeFileSync/Bin:$PATH"
-export PATH="$HOME/.poetry/bin:$PATH"
-export PATH="/opt/pycharm-community-2020.1/bin:$PATH"
+export PATH="/opt/pycharm/bin:$PATH"
 export PATH="/home/nick/projects/flutter/flutter/bin:$PATH"
 export PATH="/opt/TurboVNC/bin:$PATH"
 export ANDROID_SDK_ROOT=/home/nick/projects/Android
