0f9e2f4ddaa94cef62062f45a96f0ce30bcd0e60
Nick Engelhardt <engelhardtnick@gmail.com>
2021-01-07 13:45:00 +0100

    Remove razer directory

--- razer-specific/env_vars.zsh
+++
@@ -1,7 +0,0 @@
-export PATH="/opt/idea/bin:$PATH"
-export PATH="/opt/FreeFileSync/Bin:$PATH"
-export PATH="/opt/pycharm/bin:$PATH"
-export PATH="/home/nick/projects/flutter/flutter/bin:$PATH"
-export PATH="/opt/TurboVNC/bin:$PATH"
-export ANDROID_SDK_ROOT=/home/nick/projects/Android
-
