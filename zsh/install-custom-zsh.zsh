a4e935dbfc90e7e831bd610e25c78ffe1ef6d34a
Nick Engelhardt <engelhardtnick@gmail.com>
2021-02-03 21:25:22 +0100

    Backup old .zshrc file upon install
    Also only add syndaemon to .profile if it doesn't exist yet.

--- zsh/install-custom-zsh.zsh
+++ zsh/install-custom-zsh.zsh
@@ -1,6 +1,12 @@
 #!/bin/zsh
-
+cp /home/$USER/.zshrc /home/$USER/.zshrc_bak
+rm /home/$USER/.zshrc
 ln -s /home/$USER/projects/dev-environment/zsh/.zshrc /home/$USER/.zshrc
 
-echo "##### CUSTOM STUFF \nsyndaemon -i 0.3 -K -R -d" >> /home/$USER/.profile
+if grep -q "syndaemon" "/home/$USER/.profile";
+then
+    echo "syndaemon entry already in .profile";
+else
+    echo "##### CUSTOM STUFF \nsyndaemon -i 0.3 -K -R -d" >> /home/$USER/.profile;
+fi
 
