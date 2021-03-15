ac4ceff22af8d817449a63aba6e5e929e11304b1
Nick Engelhardt <engelhardtnick@gmail.com>
2021-03-15 20:16:02 +0100

    Add env vars for pycharm and node.js

--- xps15-specific/env_vars.zsh
+++ xps15-specific/env_vars.zsh
@@ -1,3 +1,6 @@
 export PATH="/opt/idea/bin:$PATH"
 export PATH="/opt/android-studio/bin:$PATH"
 export PATH="/home/nick/projects/flutter/bin:$PATH"
+export PATH="/opt/pycharm/bin:$PATH"
+export PATH="/opt/node/bin:$PATH"
+
