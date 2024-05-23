5385ce8bccdf73ddff3b0f0a2a31f6545dacd365
Nick Engelhardt <nick.engelhardt.external@zalando.de>
2024-05-23 09:14:43 +0200

    Configure shell history
    This adds fuzzy search via CTRL+R and timestamps as well as
    increases the history size dramatically.

--- zsh/zsh_settings.zsh
+++ zsh/zsh_settings.zsh
@@ -9,7 +9,31 @@ ENABLE_CORRECTION="false"
 # Uncomment the following line to display red dots whilst waiting for completion.
 COMPLETION_WAITING_DOTS="true"
 
-# commands prefixed with a space don't land in history
-setopt HIST_IGNORE_SPACE
+# Improve shell history as advised here https://martinheinz.dev/blog/110
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=10000000
+SAVEHIST=10000000
+HIST_STAMPS="yyyy-mm-dd"
+# Install fzf and the silver searcher:
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/fzf
+# https://github.com/junegunn/fzf?tab=readme-ov-file#using-homebrew
+# https://formulae.brew.sh/formula/the_silver_searcher
+export FZF_BASE=/opt/homebrew/opt/fzf
+export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
 
-plugins=(git zsh-completions)
+HISTORY_IGNORE="(clear|ls|pwd|exit)*"
+
+setopt EXTENDED_HISTORY      # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY    # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY         # Share history between all sessions.
+setopt HIST_IGNORE_DUPS      # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS  # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_SPACE     # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS     # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY           # Do not execute immediately upon history expansion.
+setopt APPEND_HISTORY        # append to history file (Default)
+setopt HIST_NO_STORE         # Don't store history commands
+setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
+
+
+plugins=(git zsh-completions fzf)
